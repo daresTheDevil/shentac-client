3389175d04b9a414060e665ef2b427c6ca034d1e
David Kay <davidbkay@gmail.com>
2020-01-27 21:23:27 -0600

    init commit

---
+++ layouts/default.vue
@@ -0,0 +1,135 @@
+<template>
+  <v-app>
+    <!-- <v-navigation-drawer
+      v-model="drawer"
+      :mini-variant="miniVariant"
+      :clipped="clipped"
+      fixed
+      app
+    >
+      <v-list>
+        <v-list-item
+          v-for="(item, i) in items"
+          :key="i"
+          :to="item.to"
+          router
+          exact
+        >
+          <v-list-item-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title v-text="item.title" />
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer> -->
+    <v-app-bar
+      :clipped-left="clipped"
+      fixed
+      app
+      color="blue-grey lighten-5"
+      elevate-on-scroll
+    >
+      <!-- <v-app-bar-nav-icon @click.stop="drawer = !drawer" /> -->
+      <!-- 
+      <v-btn @click.stop="miniVariant = !miniVariant" icon>
+        <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
+      </v-btn>
+      <v-btn @click.stop="clipped = !clipped" icon>
+        <v-icon>mdi-application</v-icon>
+      </v-btn>
+      <v-btn @click.stop="fixed = !fixed" icon>
+        <v-icon>mdi-minus</v-icon>
+      </v-btn> -->
+
+      <v-toolbar-title>Shenandoah Tactical</v-toolbar-title>
+      <v-spacer />
+
+      <v-toolbar-items class="hidden-sm-and-down">
+        <v-btn text>Home</v-btn>
+        <v-btn text>Calendar</v-btn>
+        <v-btn text disabled>Register</v-btn>
+      </v-toolbar-items>
+      <v-btn icon large class="hidden-sm-and-down">
+        <v-icon large color="light-blue darken-2">mdi-facebook</v-icon>
+      </v-btn>
+      <v-btn icon large class="hidden-sm-and-down">
+        <v-icon large color="red darken-2">mdi-instagram</v-icon>
+      </v-btn>
+
+      <v-btn
+        @click.stop="rightDrawer = !rightDrawer"
+        icon
+        class="hidden-sm-and-up"
+      >
+        <v-icon>mdi-menu</v-icon>
+      </v-btn>
+    </v-app-bar>
+    <v-content class="blue-grey lighten-5">
+      <nuxt />
+    </v-content>
+    <v-navigation-drawer v-model="rightDrawer" :right="right" temporary fixed>
+      <v-list>
+        <v-subheader>Social</v-subheader>
+        <v-list-item>
+          <v-list-item-action class="mr-4">
+            <v-icon large color="light-blue darken-2">
+              mdi-facebook
+            </v-icon>
+          </v-list-item-action>
+          <v-list-item-title>Connect on Facebook</v-list-item-title>
+        </v-list-item>
+        <v-list-item>
+          <v-list-item-action class="mr-4">
+            <v-icon large color="red darken-2">
+              mdi-instagram
+            </v-icon>
+          </v-list-item-action>
+          <v-list-item-title>Connect on Instagram</v-list-item-title>
+        </v-list-item>
+      </v-list>
+      <v-divider />
+      <v-list>
+        <v-subheader>Links</v-subheader>
+        <v-list-item>
+          <v-list-item-title>Our Classes</v-list-item-title>
+        </v-list-item>
+        <v-list-item>
+          <v-list-item-title>Our Services</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <!-- <v-footer :fixed="fixed" app>
+      <span>&copy; 2019</span>
+    </v-footer> -->
+  </v-app>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      clipped: false,
+      drawer: false,
+      fixed: false,
+      items: [
+        {
+          icon: 'mdi-apps',
+          title: 'Welcome',
+          to: '/'
+        },
+        {
+          icon: 'mdi-chart-bubble',
+          title: 'Inspire',
+          to: '/inspire'
+        }
+      ],
+      miniVariant: false,
+      right: true,
+      rightDrawer: false,
+      title: 'Vuetify.js'
+    }
+  }
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,269 @@
+<template>
+  <div>
+    <v-carousel :show-arrows="false" cycle>
+      <v-carousel-item
+        v-for="(item, i) in items"
+        :key="i"
+        :src="item.src"
+        gradient="to top right, rgba(38,50,56,.8), rgba(84,110,122,.4)"
+      >
+        <v-container fill-height>
+          <v-row>
+            <v-col cols="auto">
+              <h1 class="white--text display-3 font-weight-bold">
+                {{ item.title }}
+              </h1>
+              <a class="white--text headline">{{ item.text }}</a>
+            </v-col>
+          </v-row>
+        </v-container></v-carousel-item
+      >
+    </v-carousel>
+    <!-- <v-img
+      src="pistol-sig.jpg"
+      height="400"
+      gradient="to top right, rgba(100,115,201,.5), rgba(25,32,72,.9)"
+    >
+      <v-container fill-height>
+        <v-row>
+          <v-col>
+            <h1 class="display-3 white--text font-weight-bold">Safety first</h1>
+            <a href="#" class="white--text headline">
+              Learn more about our courses
+            </a>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-img> -->
+    <section class="pt-12 pb-8">
+      <v-container>
+        <v-row justify="center">
+          <v-col cols="auto">
+            <h1 class="display-2 text-center">Our Courses</h1>
+            <p style="max-width: 60ch;">
+              We teach you how to shoot guns and stuff.
+            </p>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col sm="12" md="4">
+            <v-card
+              :raised="$vuetify.breakpoint.smAndUp"
+              :outlined="$vuetify.breakpoint.smAndDown"
+            >
+              <v-card-text>
+                <img src="basic.svg" width="125" />
+              </v-card-text>
+              <v-card-text class="py-0">
+                <h1 class="headline">
+                  Basic Handgun Skills & Marksmanship
+                </h1>
+              </v-card-text>
+              <v-card-text>
+                The following is placeholder text known as “lorem ipsum,” which
+                is scrambled Latin used by designers to mimic real copy.
+                Maecenas non leo laoreet, condimentum lorem nec, vulputate
+                massa. Mauris egestas at nibh nec finibus.
+              </v-card-text>
+              <v-card-actions>
+                <v-spacer />
+                <v-btn depressed dark>Learn More</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-col>
+          <v-col sm="12" md="4">
+            <v-card
+              :raised="$vuetify.breakpoint.smAndUp"
+              :outlined="$vuetify.breakpoint.smAndDown"
+            >
+              <v-card-text>
+                <img src="advanced.svg" width="125" />
+              </v-card-text>
+              <v-card-text class="py-0">
+                <h1 class="headline">
+                  Advanced Handgun Skills & Marksmanship
+                </h1>
+              </v-card-text>
+              <v-card-text>
+                The following is placeholder text known as “lorem ipsum,” which
+                is scrambled Latin used by designers to mimic real copy.
+                Maecenas non leo laoreet, condimentum lorem nec, vulputate
+                massa. Mauris egestas at nibh nec finibus.
+              </v-card-text>
+              <v-card-actions>
+                <v-spacer />
+                <v-btn depressed dark>Learn More</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-col>
+          <v-col sm="12" md="4">
+            <v-card
+              :raised="$vuetify.breakpoint.smAndUp"
+              :outlined="$vuetify.breakpoint.smAndDown"
+            >
+              <v-card-text>
+                <img src="concealed.svg" width="125" />
+              </v-card-text>
+              <v-card-text class="py-0">
+                <h1 class="headline">
+                  Enhanced Concealed Carry for Mississippi
+                </h1>
+              </v-card-text>
+              <v-card-text>
+                The following is placeholder text known as “lorem ipsum,” which
+                is scrambled Latin used by designers to mimic real copy.
+                Maecenas non leo laoreet, condimentum lorem nec, vulputate
+                massa. Mauris egestas at nibh nec finibus.
+              </v-card-text>
+              <v-card-actions>
+                <v-spacer />
+                <v-btn depressed dark>Learn More</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+    </section>
+    <section class="py-8">
+      <v-container>
+        <v-row justify="center">
+          <v-col cols="auto">
+            <h1 class="display-2 text-center">Security Services</h1>
+            <p style="max-width: 60ch;">
+              Shenandoah Tactical has 30 years of experience in military and
+              federal law enforcement and can provide security surveys, private
+              investigative services and private security.
+            </p>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col sm="12" md="4">
+            <v-card
+              :raised="$vuetify.breakpoint.smAndUp"
+              :outlined="$vuetify.breakpoint.smAndDown"
+            >
+              <v-card-text>
+                <img src="security-survey.svg" width="125" />
+              </v-card-text>
+              <v-card-text class="py-0">
+                <h1 class="headline">
+                  Security Surveys
+                </h1>
+              </v-card-text>
+              <v-card-text>
+                The following is placeholder text known as “lorem ipsum,” which
+                is scrambled Latin used by designers to mimic real copy.
+                Maecenas non leo laoreet, condimentum lorem nec, vulputate
+                massa. Mauris egestas at nibh nec finibus.
+              </v-card-text>
+              <v-card-actions>
+                <v-spacer />
+                <v-btn depressed dark>Learn More</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-col>
+          <v-col sm="12" md="4">
+            <v-card
+              :raised="$vuetify.breakpoint.smAndUp"
+              :outlined="$vuetify.breakpoint.smAndDown"
+            >
+              <v-card-text>
+                <img src="private-eye.svg" width="125" />
+              </v-card-text>
+              <v-card-text class="py-0">
+                <h1 class="headline">
+                  Private Investigative Services
+                </h1>
+              </v-card-text>
+              <v-card-text>
+                The following is placeholder text known as “lorem ipsum,” which
+                is scrambled Latin used by designers to mimic real copy.
+                Maecenas non leo laoreet, condimentum lorem nec, vulputate
+                massa. Mauris egestas at nibh nec finibus.
+              </v-card-text>
+              <v-card-actions>
+                <v-spacer />
+                <v-btn depressed dark>Learn More</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-col>
+          <v-col sm="12" md="4">
+            <v-card
+              :raised="$vuetify.breakpoint.smAndUp"
+              :outlined="$vuetify.breakpoint.smAndDown"
+            >
+              <v-card-text>
+                <img src="security.svg" width="125" />
+              </v-card-text>
+              <v-card-text class="py-0">
+                <h1 class="headline">
+                  Private Security
+                </h1>
+              </v-card-text>
+              <v-card-text>
+                The following is placeholder text known as “lorem ipsum,” which
+                is scrambled Latin used by designers to mimic real copy.
+                Maecenas non leo laoreet, condimentum lorem nec, vulputate
+                massa. Mauris egestas at nibh nec finibus.
+              </v-card-text>
+              <v-card-actions>
+                <v-spacer />
+                <v-btn depressed dark>Learn More</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+    </section>
+
+    <section class="py-8">
+      <v-container>
+        <v-row justify="center">
+          <v-col cols="auto">
+            <h1 class="display-2 text-center">We are Shenandoah Tactical</h1>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col sm="12" md="4">
+            <v-card
+              :raised="$vuetify.breakpoint.smAndUp"
+              :outlined="$vuetify.breakpoint.smAndDown"
+            >
+              <v-img src="training-opa.jpg" />
+              <v-card-text>
+                <h1 class="title">
+                  Robert Kay
+                </h1>
+                <p>30 years of experience shooting guns and stuff.</p>
+              </v-card-text>
+              <v-card-actions>
+                <v-spacer />
+                <v-btn depressed dark>Learn More</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {},
+  data: () => ({
+    items: [
+      {
+        src: 'opa-range.jpg',
+        title: 'Self Defense',
+        text: 'We can put some text here'
+      },
+      {
+        src: 'pistol-sig.jpg',
+        title: 'Concealed Carry',
+        text: 'Some more text goes here'
+      }
+    ]
+  })
+}
+</script>
